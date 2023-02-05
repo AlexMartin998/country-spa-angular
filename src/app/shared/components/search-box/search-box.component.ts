15765d23772ce5d87540f7b1f8887d0d959f462f
Alex Martin <alextravc.342@gmail.com>
2023-02-05 18:33:45 -0500

    [ADD]: emit search term

--- src/app/shared/components/search-box/search-box.component.ts
+++ src/app/shared/components/search-box/search-box.component.ts
@@ -1,4 +1,11 @@
-import { Component, Input } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  EventEmitter,
+  Input,
+  Output,
+  ViewChild,
+} from '@angular/core';
 
 @Component({
   selector: 'shared-search-box',
@@ -7,4 +14,18 @@ import { Component, Input } from '@angular/core';
 export class SearchBoxComponent {
   @Input()
   public placeholder: string = '';
+
+  @Output()
+  public onValue = new EventEmitter<string>();
+
+  @ViewChild('txtInput') // solo a ref locales
+  public tagInput!: ElementRef<HTMLInputElement>;
+
+  emitValue() {
+    const input = this.tagInput.nativeElement;
+    if (!input.value) return;
+    this.onValue.emit(input.value);
+
+    input.value = '';
+  }
 }
